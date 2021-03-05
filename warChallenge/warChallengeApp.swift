4a6bdd3c968ead83c5c939550c31970e58dc117b
octavian-ilie <octavian.ilie@icloud.com>
2021-03-05 09:21:01 +0100

    Initial Commit

---
+++ warChallenge/warChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeApp.swift
+//  warChallenge
+//
+//  Created by Octavian Mihuț Ilie on 05/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
