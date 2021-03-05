11793edcbf36fd697847dc75a6b96b021ab87372
octavian-ilie <octavian.ilie@icloud.com>
2021-03-05 09:48:13 +0100

    Basic UI

--- warChallenge/ContentView.swift
+++ warChallenge/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea(.all)
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card12")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 20))
+                            .padding(.bottom, 5.0)
+                        Text("0")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 40))
+                    }
+                    Spacer()
+                    VStack {
+                        Text("Computer")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 20))
+                            .padding(.bottom, 5.0)
+                        Text("0")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 40))
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
