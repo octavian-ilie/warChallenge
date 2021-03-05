f4aea5d9c043217b1ba88281a00da908c69cc6ec
octavian-ilie <octavian.ilie@icloud.com>
2021-03-05 17:03:42 +0100

    Generate random cards

--- warChallenge/ContentView.swift
+++ warChallenge/ContentView.swift
@@ -8,6 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var playerName = "Octavian"
+
+    @State var playerCard: String = "card2"
+    @State var computerCard: String = "card12"
+
+    @State var playerScore = 0
+    @State var computerScore = 0
+    
+    func playHand() {
+        let cardPrefix: String = "card"
+        
+        var regenerateCards: Bool = true
+        
+        var randomPlayerCard: Int
+        var randomComputerCard: Int
+        
+        // Generate random cards, check if they're the same and regenerate if so
+        while regenerateCards {
+            randomPlayerCard = Int.random(in: 2...14)
+            randomComputerCard = Int.random(in: 2...14)
+            
+            if randomPlayerCard != randomComputerCard {
+                playerCard = cardPrefix + String(randomPlayerCard)
+                computerCard = cardPrefix + String(randomComputerCard)
+                regenerateCards = false
+            } else {
+                regenerateCards = true
+            }
+        }
+    }
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -20,23 +51,27 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card12")
+                    Image(computerCard)
                     Spacer()
                 }
                 Spacer()
-                Image("dealbutton")
+                Button(action: {
+                    playHand()
+                }, label: {
+                    Image("dealbutton")
+                })
                 Spacer()
                 HStack {
                     Spacer()
                     VStack {
-                        Text("Player")
+                        Text(playerName)
                             .fontWeight(.medium)
                             .foregroundColor(Color.white)
                             .font(.system(size: 20))
                             .padding(.bottom, 5.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .foregroundColor(Color.white)
                             .font(.system(size: 40))
                     }
@@ -47,7 +82,7 @@ struct ContentView: View {
                             .foregroundColor(Color.white)
                             .font(.system(size: 20))
                             .padding(.bottom, 5.0)
-                        Text("0")
+                        Text(String(computerScore))
                             .foregroundColor(Color.white)
                             .font(.system(size: 40))
                     }
